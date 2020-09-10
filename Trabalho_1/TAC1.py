e9fd78cac6b3dd80525b7f2353bfb4407c8dff93
Juliana-Caricio <juliana19caricio@gmail.com>
2020-09-10 20:28:55 -0300

    tac1-exercicio1

--- Trabalho_1/TAC1.py
+++ Trabalho_1/TAC1.py
@@ -2,7 +2,7 @@ refArquivo = open("\Users\Juliana\Documents\Juliana\programacao\Arquivo fasta.tx
 identificador = refArquivo.readline()[1:16]
 sequencia = ""
 for linha in refArquivo:
-    sequencia += linha.replace('\n', '')
+    sequencia += linha.replace('\t', '')
 print ("identificador: %s" % identificador)
 print ("Sequencia: %s" % sequencia)
 refArquivo.close()
