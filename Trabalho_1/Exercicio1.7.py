c56a8fcf6cad3552c456957d2aeadde898b0b1a0
Juliana-Caricio <juliana19caricio@gmail.com>
2020-09-20 15:25:35 -0300

    exercicio1

--- Trabalho_1/Exercicio1.7.py
+++ Trabalho_1/Exercicio1.7.py
@@ -1,1 +1,6 @@
-
+import pandas as pd
+df = pd.read_excel("/Users/Juliana/Documents/Juliana/programacao/Juliana Caricio - WHO POP TB some.xlsx")
+colunas = df.iloc[[1, 2, 5, 8, 10],2:3]
+soma = colunas.sum()
+media = colunas.mean(axis=0)
+print("Total de mortes dos BRICS:", soma, "\nMedia das mortes dos BRICS:", media)
