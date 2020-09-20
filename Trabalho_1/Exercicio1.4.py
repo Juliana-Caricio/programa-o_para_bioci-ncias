dec028e6d544b823d9b702b0dae02ff22e4ba2a7
Juliana-Caricio <juliana19caricio@gmail.com>
2020-09-20 14:43:38 -0300

    exercicio1

--- Trabalho_1/Exercicio1.4.py
+++ Trabalho_1/Exercicio1.4.py
@@ -1,5 +1,6 @@
 import pandas as pd
 df = pd.read_excel("/Users/Juliana/Documents/Juliana/programacao/Juliana Caricio - WHO POP TB some.xlsx")
-media = df.mean(axis=0)
-mediana = df.median(axis=0)
-print("media =", media, "mediana =", mediana)
+col = df.iloc[:, -1]
+media = col.mean(axis=0)
+mediana = col.median(axis=0)
+print("media de mortes =", media, "\nmediana de mortes =", mediana)
