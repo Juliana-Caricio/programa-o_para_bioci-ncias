cac992d45fa1d551ea596a3d4e56b41dd43d1c75
Juliana-Caricio <juliana19caricio@gmail.com>
2020-09-20 14:49:19 -0300

    exercicio1

--- Trabalho_1/Exercicio1.7.py
+++ Trabalho_1/Exercicio1.7.py
@@ -1,3 +1,6 @@
 import pandas as pd
 df = pd.read_excel("/Users/Juliana/Documents/Juliana/programacao/Juliana Caricio - WHO POP TB some.xlsx")
-
+col = df.iloc[:, -1]
+maior = col.max()
+menor = col.min()
+print("Maior numero de mortes:", maior, "\nMenor numero de mortes:", menor)
