a67386bf803d560d2039c2b7c21dc8556ceb5d79
Juliana-Caricio <juliana19caricio@gmail.com>
2020-09-23 09:45:11 -0300

    exercicio1

--- Trabalho_1/Exercicio1.6.py
+++ Trabalho_1/Exercicio1.6.py
@@ -1,7 +1,5 @@
 import pandas as pd
 df = pd.read_excel("/Users/Juliana/Documents/Juliana/programacao/Juliana Caricio - WHO POP TB some.xlsx")
-deaths = df.iloc[:, -1]
-loc = df.iloc[:, 1:2]
-div = loc.div(100)
-normal = (deaths/div)
-print(normal)
+normal = df["Population (1000s)"]/100
+deaths = df["TB deaths"]/normal
+print("Taxa de morte normalizada por 100.000 habitantes:\n", deaths)
