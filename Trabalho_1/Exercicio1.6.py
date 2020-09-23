235addd58ce9d8d38e7d00cb4485374a623453b8
Juliana-Caricio <juliana19caricio@gmail.com>
2020-09-22 21:06:30 -0300

    exercicio1

--- Trabalho_1/Exercicio1.6.py
+++ Trabalho_1/Exercicio1.6.py
@@ -1,6 +1,7 @@
 import pandas as pd
 df = pd.read_excel("/Users/Juliana/Documents/Juliana/programacao/Juliana Caricio - WHO POP TB some.xlsx")
-mult = df.mul([1, 1, 100000], axis=1)
-loc = mult.iloc[:, -1]
-div = loc.div([21472, 200362, 1393337, 757, 1704, 1252140, 25834, 10608, 142834, 193, 52776, 1133], axis=0)
-print("Taxa de morte por 100000 habitantes :\n", div)
+deaths = df.iloc[:, -1]
+loc = df.iloc[:, 1:2]
+div = loc.div(100)
+normal = (deaths/div)
+print(normal)
