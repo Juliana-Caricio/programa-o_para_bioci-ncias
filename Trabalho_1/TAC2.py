32cee58e90a43b735df278961e3581ae5110fcb8
Juliana-Caricio <juliana19caricio@gmail.com>
2020-10-01 20:21:09 -0300

    TAC2

--- Trabalho_1/TAC2.py
+++ Trabalho_1/TAC2.py
@@ -1,7 +1,20 @@
-import pandas as pd
-df = pd.read_excel("/Users/Juliana/Documents/Juliana/programacao/Valores_CT.xlsx")
-coef_m = -3.397186047
-coef_b = 58.53223295
-ct = df.iloc[:, -1]
-df["Quantity"] = 10 * ((ct - coef_b)/coef_m)
-print(df)
+def quantidade_absoluta(coef_m, coef_b, tabela):
+    import pandas as pd
+    df = pd.read_excel(tabela)
+    df["Quantity"] = 10 * ((df["CT"] - coef_b)/coef_m)
+    print(df)
+
+quantidade_absoluta(-3.397186047, 58.53223295, "/Users/Juliana/Documents/Juliana/programacao/Valores_CT.xlsx")
+
+def quant_absoluta(coef_m, coef_b, tabela):
+    import pandas as pd
+    df = pd.read_excel(tabela)
+    col = df.iloc[:, 1:3]
+    df_q = pd.DataFrame()
+    df_q["Quantity"] = 10 * ((df["CT"] - coef_b)/coef_m)
+    df_final = pd.concat([col, df_q], axis=1)
+    df_final.to_excel("/Users/Juliana/Documents/Juliana/programacao/Valores_CT.xlsx", sheet_name="Tabela")
+    print(df_final)
+
+quant_absoluta(-3.397186047, 58.53223295, "/Users/Juliana/Documents/Juliana/programacao/Valores_CT.xlsx")
+
