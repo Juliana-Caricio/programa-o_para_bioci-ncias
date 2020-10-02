69ce321db6d139515700773fe8e9077ea6c1eb3f
Juliana-Caricio <juliana19caricio@gmail.com>
2020-10-02 09:39:55 -0300

    TAC2

--- Trabalho_1/TAC2.py
+++ Trabalho_1/TAC2.py
@@ -1,20 +1,26 @@
-def quantidade_absoluta(coef_m, coef_b, tabela):
-    import pandas as pd
-    df = pd.read_excel(tabela)
-    df["Quantity"] = 10 * ((df["CT"] - coef_b)/coef_m)
-    print(df)
+import pandas as pd
+import sys
+tabela = sys.argv[1]
+coef_m = float(sys.argv[2])
+coef_b = float(sys.argv[3])
+df = pd.read_excel(tabela)
+df["Quantity"] = 10 * ((df["CT"] - coef_b)/coef_m)
+print(df)
 
-quantidade_absoluta(-3.397186047, 58.53223295, "/Users/Juliana/Documents/Juliana/programacao/Valores_CT.xlsx")
 
-def quant_absoluta(coef_m, coef_b, tabela):
-    import pandas as pd
-    df = pd.read_excel(tabela)
-    col = df.iloc[:, 1:3]
-    df_q = pd.DataFrame()
-    df_q["Quantity"] = 10 * ((df["CT"] - coef_b)/coef_m)
-    df_final = pd.concat([col, df_q], axis=1)
-    df_final.to_excel("/Users/Juliana/Documents/Juliana/programacao/Valores_CT.xlsx", sheet_name="Tabela")
-    print(df_final)
 
-quant_absoluta(-3.397186047, 58.53223295, "/Users/Juliana/Documents/Juliana/programacao/Valores_CT.xlsx")
+import pandas as pd
+import sys
+tabela = sys.argv[1]
+coef_m = float(sys.argv[2])
+coef_b = float(sys.argv[3])
+df = pd.read_excel(tabela)
+col = df.iloc[:, 1:3]
+df_q = pd.DataFrame()
+df_q["Quantity"] = 10 * ((df["CT"] - coef_b)/coef_m)
+qf_final = pd.concat([col, df_q], axis=1)
+df_final.to_excel("/Users/Juliana/Documents/Juliana/programacao/Valores_CT.xlsx", sheet_name="Tabela")
+print(df_final)
+
+python teste.py/Users/Juliana/Documents/Juliana/programacao/Valores_CT.xlsx -3.4 58.5
 
