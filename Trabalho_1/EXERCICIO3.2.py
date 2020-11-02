4f4a6207d78ec1a4c844a6527e4a1ac75425abce
Juliana-Caricio <juliana19caricio@gmail.com>
2020-11-02 12:51:14 -0300

    exercicio3

--- Trabalho_1/EXERCICIO3.2.py
+++ Trabalho_1/EXERCICIO3.2.py
@@ -1,8 +1,8 @@
 nome = input("Digite seu nome completo:")
-nota_1 = input("Digite 1º nota:")
-nota_2 = input("Digite 2° nota:")
-nota_3 = input("Digite 3° nota:")
-faltas = input("Digite numero de faltas:")
+nota_1 = float(input("Digite 1º nota:"))
+nota_2 = float(input("Digite 2° nota:"))
+nota_3 = float(input("Digite 3° nota:"))
+faltas = float(input("Digite numero de faltas:"))
 media = (nota_1 + nota_2 + nota_3)/3
 p_faltas = (faltas*100)/80
 if p_faltas <= 25 and media >= 7.0:
