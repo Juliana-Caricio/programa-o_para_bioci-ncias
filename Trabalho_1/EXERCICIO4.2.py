0e55eea3b09c559f6db4ecdb3bb4d59867b309c8
Juliana-Caricio <juliana19caricio@gmail.com>
2020-11-10 13:53:30 -0300

    exercicio4

--- Trabalho_1/EXERCICIO4.2.py
+++ Trabalho_1/EXERCICIO4.2.py
@@ -1,7 +1,6 @@
 def calculaAreaRetangulo():
     a = int(input("Digite lado do retangulo:"))
     b = input("Digite outro lado do retangulo:")
-    area = 0
     if b == "":
         area = a*a
     else:
