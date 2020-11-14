06290d47f99f3ec01ace2fdcef82fec45dafa9fb
Juliana-Caricio <juliana19caricio@gmail.com>
2020-11-13 21:35:32 -0300

    projeto_final

--- Trabalho_1/Projeto_Final.py
+++ Trabalho_1/Projeto_Final.py
@@ -1,9 +1,21 @@
 import pandas as pd
 tabela = pd.read_excel("/Users/Juliana/Documents/Juliana/programacao/Tabela_1.xlsx")
 coluna1 = tabela.iloc[:,1]
+coluna2 = tabela.iloc[:,2]
+coluna3 = tabela.iloc[:,3]
+coluna4 = tabela.iloc[:,4]
 soma1 = sum(coluna1)
-coluna_nova = pd.DataFrame()
-coluna_nova["Rap_1A_Normal"] = (coluna1*1000000)/soma1
-tabela_nova = pd.concat([tabela,coluna_nova], axis=1)
+soma2 = sum(coluna2)
+soma3 = sum(coluna3)
+soma4 = sum(coluna4)
+coluna_nova1 = pd.DataFrame()
+coluna_nova1["Rap_1A_Normal"] = (coluna1*1000000)/soma1
+coluna_nova2 = pd.DataFrame()
+coluna_nova2["Rap_2A_Normal"] = (coluna2*1000000)/soma2
+coluna_nova3 = pd.DataFrame()
+coluna_nova3["Rap_1B_Normal"] = (coluna3*1000000)/soma3
+coluna_nova4 = pd.DataFrame()
+coluna_nova4["Rap_2B_Normal"] = (coluna4*1000000)/soma4
+tabela_nova = pd.concat([tabela,coluna_nova1, coluna_nova2, coluna_nova3, coluna_nova4], axis=1)
 print(tabela_nova)
 
