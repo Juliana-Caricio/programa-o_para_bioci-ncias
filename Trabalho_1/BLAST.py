e9cbf217684d1366f519e16846cb0968d9ec2d4e
Juliana-Caricio <juliana19caricio@gmail.com>
2021-01-26 16:41:37 -0300

    tac21

--- Trabalho_1/BLAST.py
+++ Trabalho_1/BLAST.py
@@ -1,12 +1,9 @@
-from Bio import SeqIO
-from Bio.Blast.Applications import NcbiblastxCommandline
-records = SeqIO.index("Rdesconhecidus.fasta", "fasta")
-x = records.get_raw("gene_1").decode()
-y = str(x)
-blastx = "/Users/Juliana/Documents/Juliana/programacao/blastx.exe"
-txt = "/Users/Juliana/Documents/Juliana/programacao/blast.txt"
-comando_blastx = NcbiblastxCommandline(cmd= blastx, query= x, subject="VectorBase-48_RprolixusCDC_AnnotatedProteins.fasta", outfmt=0, out="blast.txt")
-stdout, stderr = comando_blastx()
-blast_result = open("blast.txt", "r")
-linhas = blast_result.read()
-print(linhas)
+homo = input("Sequencia de DNA: ")
+lista = list(homo)
+i = 0
+j = 1
+while i<= len(lista)-1:
+    if lista[x] = lista[x-j]:
+        print(lista[x])
+
+
